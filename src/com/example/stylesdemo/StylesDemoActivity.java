e00b7bbde4e16a894581a6572c391aa937370715
Karthik Sirasanagandla <sirkarthik@gmail.com>
2016-03-12 23:33:49 +0530

    Initial Commit

---
+++ src/com/example/stylesdemo/StylesDemoActivity.java
@@ -0,0 +1,15 @@
+package com.example.stylesdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class StylesDemoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_styles_demo);
+	}
+}
